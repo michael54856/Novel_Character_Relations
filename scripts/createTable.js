47493b6e3b963cd3be0f448cfeaaf3779e50aeab
michael54856 <michael548562@gmail.com>
2021-07-28 23:51:49 +0800

    change get JSON link

--- scripts/createTable.js
+++ scripts/createTable.js
@@ -4,7 +4,7 @@ let nodeBtn = document.getElementById("nodeButton");
 nodeBtn.addEventListener("click", function(){
     let tableContainer = document.getElementById("myTable");
     let tableHTML = "<table><thead><tr><th>名次</th><th>人物</th><th>重要程度</th></tr></thead>";
-    $.getJSON("../data.json", function(data)
+    $.getJSON("https://michael54856.github.io/Novel_Character_Relations/data.json", function(data)
     {
         tableHTML += "<tbody>";
         $.each(data["nodes"],function(i,item)
@@ -27,7 +27,7 @@ let edgeBtn = document.getElementById("edgeButton");
 edgeBtn.addEventListener("click", function(){
     let tableContainer = document.getElementById("myTable");
     let tableHTML = "<table><thead><tr><th>名次</th><th>人物(1)</th><th>人物(2)</th><th>關聯次數</th></tr></thead>";
-    $.getJSON("../data.json", function(data)
+    $.getJSON("https://michael54856.github.io/Novel_Character_Relations/data.json", function(data)
     {
         tableHTML += "<tbody>";
         $.each(data["links"],function(i,item)
